949b4fea2245d91a7ee6cd9a3dfb1c731578d459
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2022-07-31 11:13:34 -0300

    add mixer benchmark

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,6 +1,23 @@
-use audio_engine::{OggDecoder, SoundSource, WavDecoder};
+use audio_engine::{Mixer, OggDecoder, SoundSource, WavDecoder};
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::io::Cursor;
+
+struct Nop;
+impl SoundSource for Nop {
+    fn channels(&self) -> u16 {
+        1
+    }
+
+    fn sample_rate(&self) -> u32 {
+        44100
+    }
+
+    fn reset(&mut self) {}
+
+    fn write_samples(&mut self, buffer: &mut [i16]) -> usize {
+        buffer.len()
+    }
+}
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("ogg decoder", |b| {
@@ -16,6 +33,20 @@ fn criterion_benchmark(c: &mut Criterion) {
                 WavDecoder::new(Cursor::new(&include_bytes!("../examples/ilussion.wav")[..]))
                     .unwrap();
             while decoder.write_samples(&mut [0; 1000][..]) != 0 {}
+        })
+    });
+
+    c.bench_function("mixer", |b| {
+        b.iter(|| {
+            let mut mixer = Mixer::new(1, audio_engine::SampleRate(44100));
+            for _ in 0..1000 {
+                let id = mixer.add_sound(
+                    (),
+                    criterion::black_box(Box::new(Nop) as Box<dyn SoundSource + Send>),
+                );
+                mixer.play(id);
+            }
+            mixer.write_samples(&mut [0; 1 << 15]);
         })
     });
 }
